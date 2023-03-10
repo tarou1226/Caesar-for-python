15940c490343fb2398baa0062022e3e747876fa1
tarou <teroro1226@outlook.jp>
2023-03-10 19:59:16 +0000

    feat: Implementation of functions corresponding to N

--- main.py
+++ main.py
@@ -12,11 +12,25 @@ def encrypt(plain_text: str) -> list:
         encryption.append(chr(ord(character) - 3))
     return encryption
 
+def encrypt_n(plain_text: str, n: int) -> list:
+    # print("hello, {}!".format(plain_text))
+    encryption = list()
+    for character in plain_text:
+        encryption.append(chr(ord(character) - n))
+    return encryption
+
 def decrypt(encryption_str: str) -> list:
     # print("hello, {}!".format(encryption_str))
     decryption = list()
     for character in encryption_str:
         decryption.append(chr(ord(character) + 3))
+    return decryption
+
+def decrypt_n(encryption_str: str, n: int) -> list:
+    # print("hello, {}!".format(encryption_str))
+    decryption = list()
+    for character in encryption_str:
+        decryption.append(chr(ord(character) + n))
     return decryption
 
 if __name__ == '__main__':
